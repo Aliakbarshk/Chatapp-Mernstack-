39d38d4b1b2c7db7981f6ff74ff7c40b148c6cf9
Aliakbarshk <aliakbarsk05@gmail.com>
2025-07-28 19:08:22 +0530

    added tailwind css

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const App = () => {
+    return(
+        <>
+        <div className="main">
+            <div className="container"></div>
+        </div>
+        
+        
+        </>
+    );
+ }
+
+ export default App;
